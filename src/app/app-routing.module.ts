39d2045884fec2814e1ad3ddb290f37f7a6f55f3
Ali <ali97arjdal@gmail.com>
2023-02-13 04:19:00 +0100

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import {ProductsComponent} from "./products/products.component";
 import {CustomersComponent} from "./customers/customers.component";
 import {BillComponent} from "./bill/bill.component";
 import {FullBillComponent} from "./full-bill/full-bill.component";
+import {AuthGuard} from "./guards/security.guard";
 
 const routes: Routes = [
-  {path: "products", component: ProductsComponent},
-  {path: "customers", component: CustomersComponent},
-  {path: "bills/:customerId", component: BillComponent},
-  {path: "bills/fullBill/:billId", component: FullBillComponent}
+  {path: "products", component: ProductsComponent, canActivate : [AuthGuard], data :{'roles':['ADMIN']}},
+  {path: "customers", component: CustomersComponent, canActivate : [AuthGuard], data :{'roles':['ADMIN']}},
+  {path: "bills/:customerId", component: BillComponent, canActivate : [AuthGuard], data :{'roles':['ADMIN']}},
+  {path: "bills/fullBill/:billId", component: FullBillComponent, canActivate : [AuthGuard], data :{'roles':['ADMIN']}}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +8,25 @@ import {HttpClientModule} from "@angular/common/http";
 import { CustomersComponent } from './customers/customers.component';
 import { BillComponent } from './bill/bill.component';
 import { FullBillComponent } from './full-bill/full-bill.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
 
+export function kcFactory(kcSecService : KeycloakService){
+  return ()=>{
+    console.log("Keycloak Initialization ...")
+    kcSecService.init({
+      config : {
+        url : "http://localhost:8080",
+        realm : "ecom-realm",
+        clientId : "ecom-client"
+      },
+      loadUserProfileAtStartUp : true,
+      initOptions : {
+        onLoad : 'check-sso',
+        checkLoginIframe : true
+      }
+    });
+  }
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,9 +38,12 @@ import { FullBillComponent } from './full-bill/full-bill.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {provide : APP_INITIALIZER, deps : [KeycloakService], useFactory : kcFactory, multi : true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
